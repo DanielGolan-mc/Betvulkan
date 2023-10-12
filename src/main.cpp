27b69be8d1bff836a40cfc0d5226d3e4d285b386
DanielGolan-mc <paradisbloopi@gmail.com>
2023-10-12 23:56:09 +0300

    Removed makefile test code; Window now opens.

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,67 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t InitialWidth = 800;
+const uint32_t InitialHeight = 600;
+const char* EngineName = "Betvulkan";
+
+void logGlfwError(int error, const char* errorMessage) {
+        std::cerr << "GLFW Error " << error << ": " << errorMessage << std::endl;
+}
+
+class Application {
+	public:
+		void invoke() {
+			initializeGlfw();
+			initializeVulkan();
+			run();
+			finalizeGlfw();
+		}
+
+	private:
+		GLFWwindow* window;
+
+		void initializeGlfw() {
+			glfwSetErrorCallback(logGlfwError);
+
+			glfwInit();
+
+			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // disable opengl
+			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // TODO: support resizable windows
+
+			window = glfwCreateWindow(InitialWidth, InitialHeight, EngineName, nullptr, nullptr);
+			//                        width         height         title       monitor  opengl-only
+		}
+
+		void initializeVulkan() { }
+
+		void run() {
+			while (!glfwWindowShouldClose(window)) {
+				glfwPollEvents();
+			}
+		}
+
+		void finalizeGlfw() {
+			glfwDestroyWindow(window);
+
+			glfwTerminate();
+		}
+};
 
 int main() {
-    glfwInit();
+	Application app;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	try {
+		app.invoke();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+	return EXIT_SUCCESS;
 }
+
